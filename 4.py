9191d85bd76cdedd60c1d09c1635bdcc82d3a058
angelsvech <159538209+angelsvech@users.noreply.github.com>
2024-03-21 20:05:56 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,46 @@
+def zadacha1():
+    a=int(input("введите число"))
+    if a % 3 == 0:
+        print("делится")
+    else:
+        print("неделится")
+
+def zadacha2():
+    try:
+        a = int(input("введите число"))
+        s=100/a
+        print(s)
+    except ValueError:
+        print("введите пожалйсто число")
+    except ZeroDivisionError:
+        print("введите пожалйсто не 0 ")
+
+
+from datetime import datetime
+def zadacha3():
+    data=input("ввелите дату дд.мм.гггг:")
+    da=datetime.strptime(data,'%d.%m.%Y')
+    day=da.day
+    month=da.month
+    year=da.year
+
+    if day*month==year%100:
+        print("магическая дата")
+    else:
+        print("не магическая дата")
+
+def zadacha4():
+    a = input("введите четный билет")
+    bilet=list(map(int,a))
+    delim=len(a)//2
+    first=bilet[:delim]
+    second=bilet[delim:]
+
+    sf=sum(first)
+    ss=sum(second)
+
+    if sf == ss:
+        print("счастливый билет")
+    else:
+        print("не счастливый билет")
+zadacha4()
